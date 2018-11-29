6b1ff017c06c0dcd9a42b9225194b438f906dce0
Tomas Smilgius <tsmi@danskebank.lt>
2018-11-29 15:44:36 +0200

    aproval card added

--- src/index.js
+++ src/index.js
@@ -2,27 +2,36 @@ import  React from 'react';
 import  ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import AprovalCard from './AprovalCard';
 
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author='Sam'
-                timeAgo='Today at 4:45PM'
-                comment='blog post number 1'
-                avatar = {faker.image.avatar()}/>
-            <CommentDetail
-                author='Tom'
-                timeAgo='Today at 2:17PM'
-                comment='blog post number 2'
-                avatar = {faker.image.avatar()}/>
-            <CommentDetail 
-                author='Jane'
-                timeAgo='Today at 3:56PM' 
-                comment='blog post number 3' 
-                avatar = {faker.image.avatar()}/>
+            <AprovalCard>
+                <CommentDetail
+                    author='Sam'
+                    timeAgo='Today at 4:45PM'
+                    comment='blog post number 1'
+                    avatar = {faker.image.avatar()}/>
+            </AprovalCard>
+            
+            <AprovalCard>
+                <CommentDetail
+                    author='Tom'
+                    timeAgo='Today at 2:17PM'
+                    comment='blog post number 2'
+                    avatar = {faker.image.avatar()}/>
+            </AprovalCard>
+            
+            <AprovalCard>
+                <CommentDetail 
+                    author='Jane'
+                    timeAgo='Today at 3:56PM' 
+                    comment='blog post number 3' 
+                    avatar = {faker.image.avatar()}/>
+            </AprovalCard>
         </div>
     )
 };
